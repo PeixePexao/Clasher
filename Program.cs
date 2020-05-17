78ed4ab4fac5c381414314a3afee3d7e48cc625c
PeixePexao <64754887+PeixePexao@users.noreply.github.com>
2020-05-17 00:13:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Transactions;
+
+namespace Clasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double[] quadrados = new double[10000];
+            double[] xvxmu = new double[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                quadrados[i] = Math.Pow(i, 2);
+                
+            }
+            Console.WriteLine("Quadrados Gerados!");
+            System.Threading.Thread.Sleep(2000);
+            for (int i = 0; i < 10000; i++)
+            {
+                xvxmu[i] = Math.Pow(i, 2) + Math.Pow(i - 1, 2);
+            }
+            Console.WriteLine("x² + (x - 1)² gerados!");
+            System.Threading.Thread.Sleep(2000);
+            var intersect = quadrados.Intersect(xvxmu);
+
+            foreach(double res in intersect)
+            {
+                Console.WriteLine(res);
+            }
+
+            
+        }
+
+    }
+}
